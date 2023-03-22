561af8913789666cf0cda351f65d47b98292b9c4
seun ajayi <oyinkansola_aa@yahoo.com>
2023-03-22 11:48:40 -0400

    peer review changes

--- Terraform-backend/local-values.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  owners = var.engr
-  environment = var.environment
-  name = "${var.environment}-${var.engr}"
-  common_tags = {
-    owners = local.owners
-    environment = local.environment
-  }
-}
--- Terraform-backend/s3-modules.tf
+++ Terraform-backend/s3-modules.tf
@@ -1,46 +1,21 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "bootcamp-25-oyin"                 
-  tags = local.common_tags
-}
-
-resource "aws_s3_bucket_acl" "my_acl" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  acl = var.access
-}
-
-resource "aws_dynamodb_table" "tf_lock" {
-  name = "terraform-lock"
-  hash_key = "LockID"
-  read_capacity = 3
-  write_capacity = 3
-  attribute {
-     name = "LockID"
-     type = "S"
-   }
-  tags = {
-    Name = "Terraform Lock Table"
-   }
- }
-
- resource "aws_s3_bucket_versioning" "my_versioncontrol" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  versioning_configuration {
-    status = var.versioning
-  }
-}
-
 resource "aws_kms_key" "backendKMS" {
   description      = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = var.kms_window
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "config" {
-  bucket = aws_s3_bucket.s3_bucket.id
+resource "random_integer" "random" {
+  min = 1
+  max = 50000
+}
 
-  rule {
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "bootcamp-${random_integer.random.result}-${var.name}" 
+
+  server_side_encryption_configuration {
+    rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.backendKMS.arn
       sse_algorithm     = var.algorithm
      }
    }
- }
+  }        
+}
